884788b52aca62784667b09cde821d823952cf52
Security Raven <43562687+secraven@users.noreply.github.com>
2020-01-13 16:36:33 -0800

    Add files via upload

--- mac_changer.py
+++ mac_changer.py
@@ -1,5 +1,56 @@
 #!/usr/bin/env python
 
+# Must be run using python 3 - Python 2 uses raw_input()
 import subprocess
+import optparse
 
-subprocess.call("ifconfig", shell=True)
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguements) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more info.")
+    elif not options.new_mac:
+        parser.error("[-] Please specify an MAC address, use --help for more info.")
+    return options
+
+def change_mac(interface, new_mac):
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
+
+
+options = get_arguments()
+change_mac(options.interface, options.new_mac)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# interface = input("interface > ")
+# new_mac = input("new MAC > ")
+
+#interface = options.interface
+#new_mac = options.new_mac
+
+# original_mac = "08:00:27:3e:b5:fc"
+
+# subprocess.call("ifconfig " + interface + " down", shell=True)
+# subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+# subprocess.call("ifconfig " + interface + " up", shell=True)
+
+
